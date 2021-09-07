ec7b9de4d68bf51f5c4a8d507bb5665c81db169b
PechScript <90135306+PechScript@users.noreply.github.com>
2021-09-07 21:20:11 +0700

    Update conexao.sh

--- Modules/conexao.sh
+++ Modules/conexao.sh
@@ -141,7 +141,7 @@ echo -e "http_port $Pts" >> $var_sqd
 done
 echo -e "
 #Nome squid
-visible_hostname PHCN-VPN BY PECH CHHEAN - KAMPONG CHHNANG
+visible_hostname PHCN-VPN-BY-PECH_CHHEAN
 via off
 forwarded_for off
 pipeline_prefetch off" >> $var_sqd
